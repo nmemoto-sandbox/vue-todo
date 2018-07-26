df8bf72b798a9b5a1073704c31a010aede9feb2a
Takafumi Umemoto <umecch@gmail.com>
2018-07-27 00:00:37 +0900

    computedを使用してdoneの表示を変更

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
-        {{ todo.title }} - {{ todo.done }}
+        {{ todo.title }} - {{ showDone }}
     </div>
 </template>
 <script>
 export default {
-    props: ['todo']
+    props: ['todo'],
+    computed: {
+        showDone() {
+            return this.todo.done ? "完了" : "未完了"
+        }
+    }
 }
 </script>
