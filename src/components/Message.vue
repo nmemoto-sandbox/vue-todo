8f9e9ddac65f8db3a9626e63e0673c136f938e78
Takafumi Umemoto <umecch@gmail.com>
2018-07-28 00:49:22 +0900

    todoの追加と未入力時のメッセージ表示を追加

---
+++ src/components/Message.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="danger">
+        {{ message }}
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        message: String
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .danger {
+        color: red
+    }
+</style>
+
+
