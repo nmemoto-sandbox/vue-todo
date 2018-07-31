ef254ebc4c21dfe8d5b67846d06d642d82de4e73
Takafumi Umemoto <umecch@gmail.com>
2018-08-01 00:38:11 +0900

    メインコンテンツ以外のマテリアルデザイン化を実装

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,59 @@
 <template>
-  <div id="app">
-    <message></message>
-    <add-todo></add-todo>
-    <todo-list></todo-list>
-  </div>
+    <v-app id="inspire">
+      <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        app
+      >
+        <v-list dense>
+            <v-list-tile>
+                <v-list-tile-action>
+                    <v-icon>home</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Home</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Vue Todo</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+            <v-layout
+                justify-center
+                align-center
+            >
+                <v-flex text-xs-center>
+                    <message></message>
+                    <add-todo></add-todo>
+                    <todo-list></todo-list>
+                </v-flex>
+            </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer color="indigo" app>
+        <span class="white--text">&copy; 2018</span>
+      </v-footer>
+    </v-app>
 </template>
-
 <script>
 import TodoList from './components/TodoList.vue'
 import AddTodo from './components/AddTodo.vue'
 import Message from './components/Message.vue'
 export default {
-  name: 'app',
-  components: {
-    TodoList,
-    AddTodo,
-    Message
-  }
+    data () {
+      return {
+        drawer: null
+      }
+    },
+    components: {
+      TodoList,
+      AddTodo,
+      Message
+    }
 }
+
 </script>
